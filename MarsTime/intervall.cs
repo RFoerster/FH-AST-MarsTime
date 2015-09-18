b497f928b763af37dc145963fe8c3dc8cacf7df6
RFoerster <robert.jun@foerster.co.at>
2015-09-18 12:10:27 +0200

    Added Classes

---
+++ MarsTime/intervall.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsTime
+{
+    class intervall
+    {
+        private time _from;
+        private time _to;
+
+        public time From
+        {
+            get { return _from; }
+            set { _from = value; }
+        }
+
+        public time To
+        {
+            get { return _to; }
+            set { _to = value; }
+        }
+
+        public intervall(time f, time t)
+        {
+
+        }
+
+    }
+}
---
+++ MarsTime/time.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsTime
+{
+    class time
+    {
+        private int _mhour;
+        private int _mminute;
+
+        
+
+        public int Mhour
+        {
+            get { return _mhour; }
+            set { _mhour = value; }
+        }
+
+        public int Mminute
+        {
+            get { return _mminute; }
+            set { _mminute = value; }
+        }
+
+        public time(int h, int min)
+        {
+            Mhour = h;
+            Mminute = min;
+        }
+
+        
+    }
+}
